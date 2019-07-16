c1a78a0ac0fb49dad77b0c6aff700e437e876a87
19jacobk <jacobjk01@gmail.com>
2019-07-16 02:10:39 -0700

    project attached

---
+++ arufree/arufree/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  arufree
+//
+//  Created by Jacob Kim on 7/16/19.
+//  Copyright © 2019 Jacob Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
